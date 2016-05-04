84efc74ff8132f196da115a766f0aba44863e823
Connor Peshek <Connorpeshek@gmail.com>
2016-05-04 17:48:23 -0500

    add basic code

--- app.js
+++ app.js
@@ -2,8 +2,18 @@
   if (!window.addEventListener) return // Check for IE9+
 
   let options = INSTALL_OPTIONS
+  let element
+
+  particlesJS.load("particles-js", "./vendor/particles.json", function () {
+    console.log("callback - particles.js config loaded")
+  })
 
   function updateElement() {
+    element = Eager.createElement(document.body, element)
+    const eagerParticles = document.createElement("div")
+
+    eagerParticles.id = "particles-js"
+    element.appendChild(eagerParticles)
   }
 
   if (document.readyState === "loading") {
--- build/app.js
+++ build/app.js
@@ -4,8 +4,19 @@
   if (!window.addEventListener) return; // Check for IE9+
 
   var options = INSTALL_OPTIONS;
+  var element = void 0;
 
-  function updateElement() {}
+  particlesJS.load("particles-js", "./vendor/particles.json", function () {
+    console.log("callback - particles.js config loaded");
+  });
+
+  function updateElement() {
+    element = Eager.createElement(document.body, element);
+    var eagerParticles = document.createElement("div");
+
+    eagerParticles.id = "particles-js";
+    element.appendChild(eagerParticles);
+  }
 
   if (document.readyState === "loading") {
     document.addEventListener("DOMContentLoaded", updateElement);
