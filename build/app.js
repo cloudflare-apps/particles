239b140732fce4126e8a10eeec2d687145cbec25
Connor Peshek <Connorpeshek@gmail.com>
2016-05-05 13:32:29 -0500

    remove quotes and unneeded ternary operator for interactivity

--- build/app.js
+++ build/app.js
@@ -12,119 +12,115 @@
     element.id = CONTAINER_ID;
     element.style.backgroundColor = options.backgroundColor;
 
-    var interactable = void 0;
-
-    options.interactable ? interactable = "window" : interactable = "canvas";
-
     window.particlesJS(CONTAINER_ID, {
-      "particles": {
-        "number": {
-          "value": 80,
-          "density": {
-            "enable": true,
-            "value_area": 800
+      particles: {
+        number: {
+          value: 80,
+          density: {
+            enable: true,
+            value_area: 800
           }
         },
-        "color": {
-          "value": "" + options.particleColor
+        color: {
+          value: options.particleColor
         },
-        "shape": {
-          "type": "circle",
-          "stroke": {
-            "width": 0,
-            "color": "#000000"
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 0,
+            color: "#000000"
           },
-          "polygon": {
-            "nb_sides": 5
+          polygon: {
+            nb_sides: 5
           },
-          "image": {
-            "src": "img/github.svg",
-            "width": 100,
-            "height": 100
+          image: {
+            src: "img/github.svg",
+            width: 100,
+            height: 100
           }
         },
-        "opacity": {
-          "value": 0.5,
-          "random": false,
-          "anim": {
-            "enable": false,
-            "speed": 1,
-            "opacity_min": 0.1,
-            "sync": false
+        opacity: {
+          value: 0.5,
+          random: false,
+          anim: {
+            enable: false,
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false
           }
         },
-        "size": {
-          "value": 3,
-          "random": true,
-          "anim": {
-            "enable": false,
-            "speed": 40,
-            "size_min": 0.1,
-            "sync": false
+        size: {
+          value: 3,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 40,
+            size_min: 0.1,
+            sync: false
           }
         },
-        "line_linked": {
-          "enable": true,
-          "distance": 150,
-          "color": "" + options.particleColor,
-          "opacity": 0.4,
-          "width": 1
+        line_linked: {
+          enable: true,
+          distance: 150,
+          color: options.particleColor,
+          opacity: 0.4,
+          width: 1
         },
-        "move": {
-          "enable": true,
-          "speed": 6,
-          "direction": "none",
-          "random": false,
-          "straight": false,
-          "out_mode": "" + options.outOrBounce,
-          "bounce": false,
-          "attract": {
-            "enable": false,
-            "rotateX": 600,
-            "rotateY": 1200
+        move: {
+          enable: true,
+          speed: 6,
+          direction: "none",
+          random: false,
+          straight: false,
+          out_mode: options.outOrBounce,
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200
           }
         }
       },
-      "interactivity": {
-        "detect_on": "" + interactable,
-        "events": {
-          "onhover": {
-            "enable": options.interactable,
-            "mode": "" + options.onHover //options.onhover
+      interactivity: {
+        detect_on: "window",
+        events: {
+          onhover: {
+            enable: options.interactable,
+            mode: options.onHover
           },
-          "onclick": {
-            "enable": options.interactable,
-            "mode": "" + options.onClick //options.onclick
+          onclick: {
+            enable: options.interactable,
+            mode: options.onClick
           },
-          "resize": options.interactable
+          resize: options.interactable
         },
-        "modes": {
-          "grab": {
-            "distance": 400,
-            "line_linked": {
-              "opacity": 1
+        modes: {
+          grab: {
+            distance: 400,
+            line_linked: {
+              opacity: 1
             }
           },
-          "bubble": {
-            "distance": 400,
-            "size": 40,
-            "duration": 2,
-            "opacity": 8,
-            "speed": 3
+          bubble: {
+            distance: 400,
+            size: 40,
+            duration: 2,
+            opacity: 8,
+            speed: 3
           },
-          "repulse": {
-            "distance": 200,
-            "duration": 0.4
+          repulse: {
+            distance: 200,
+            duration: 0.4
           },
-          "push": {
-            "particles_nb": 4
+          push: {
+            particles_nb: 4
           },
-          "remove": {
-            "particles_nb": 2
+          remove: {
+            particles_nb: 2
           }
         }
       },
-      "retina_detect": true
+      retina_detect: true
     });
   }
 
